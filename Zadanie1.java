8cfc4a4a9a4a267cdb558132b700091f7ccfe229
NotOrangeJuice <136957521+NotOrangeJuice@users.noreply.github.com>
2025-09-18 01:23:06 +0300

    1 задание
    1 задание

---
+++ Zadanie1.java
@@ -0,0 +1,12 @@
+public class Zadanie1 {
+
+    public static void printThreeWords() {
+        System.out.print("Orange\n");
+        System.out.print("Banana\n");
+        System.out.print("Apple\n");
+    }
+
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+}
